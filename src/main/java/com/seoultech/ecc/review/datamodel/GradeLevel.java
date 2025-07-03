cfb90f7a61f25489962012216630c50c81a8b2d2
Hyo <lovellehyo@gmail.com>
2025-07-03 23:35:59 +0900

    (토큰 90% 이상 절약) ReviewTest ai 채점 response 구조 수정

--- src/main/java/com/seoultech/ecc/review/datamodel/GradeLevel.java
+++ src/main/java/com/seoultech/ecc/review/datamodel/GradeLevel.java
@@ -3,5 +3,14 @@ package com.seoultech.ecc.review.datamodel;
 public enum GradeLevel {
     CORRECT,    // 완전 정답
     PARTIAL,    // 부분 점수
-    INCORRECT   // 오답
+    INCORRECT;   // 오답
+
+    public static GradeLevel fromIntGrade(Integer grade) {
+        return switch (grade) {
+            case 2 -> GradeLevel.CORRECT;
+            case 1 -> GradeLevel.PARTIAL;
+            case 0 -> GradeLevel.INCORRECT;
+            default -> GradeLevel.INCORRECT;
+        };
+    }
 }
